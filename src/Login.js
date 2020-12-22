e6f6af64c42ef09455f79b818904ce84f7aef379
Thomas Vandenbussche <thomas.vandenbussche@outlook.com>
2020-12-22 11:03:20 +0100

    firebase auth in progress

--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,39 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './Login.css';
 import logo from './logo.png';
+import { auth } from "./firebase";
 
 function Login() {
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
 
     const signIn = e => {
         e.preventDefault()
-
-        // firebase part
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/')
+            })
+            .catch(error => alert(error.message))
     }
 
     const register =e => {
         e.preventDefault()
-
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                // creates a user with email and password
+                console.log(auth);
+                if (auth) { 
+                    history.push('/')
+                }
+            })
+            .catch(error => alert(error.message))
         // firebase part
     }
-    
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     
     return (
     <div className="login">
